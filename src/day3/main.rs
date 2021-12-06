3a1bef89cec7fbf749bba0c229926e3b841499e9
Kyle Olson <kyle.olson22@gmail.com>
2021-12-06 17:01:24 -0600

    day3 completion

--- src/day3/main.rs
+++ src/day3/main.rs
@@ -1,32 +1,83 @@
-fn main() {
-    let mut pivot: Vec<Vec<bool>> = vec![];
-    let lines: Vec<&str> = include_str!("./day3.test").lines().collect();
-    match lines.first() {
-        Some(line) => {
-            for _ in 0..line.len() {
-                pivot.push(vec![]);
-            }
-        },
-        None => panic!("No input file found"),
+#![feature(drain_filter)]
+use std::{cmp::Ordering, ops::Add, str::FromStr, string::ParseError};
+
+#[derive(Copy, Clone)]
+enum Mode {
+    MostCommon,
+    LeastCommon,
+}
+
+fn parse_input(input: &str) -> Result<Vec<String>, ParseError> {
+    input.lines().map(FromStr::from_str).collect()
+}
+
+fn part1(numbers: &[String]) -> usize {
+    let mut final_bin = String::new();
+    for i in 0..numbers[0].len() {
+        let count_one = numbers.iter().filter(|x| x[i..].starts_with('1')).count();
+        let count_zero = numbers.len() - count_one;
+
+        let current_bit_mask = match count_one.cmp(&count_zero) {
+            Ordering::Less => '0',
+            Ordering::Equal | Ordering::Greater => '1',
+        };
+
+        final_bin.push(current_bit_mask);
+    }
+
+    let inv_final_bin = final_bin.chars().fold(String::new(), |inv_final_bin, c| {
+        if c == '1' {
+            inv_final_bin.add("0")
+        } else {
+            inv_final_bin.add("1")
+        }
+    });
+    let gamma_rate = usize::from_str_radix(&final_bin, 2).unwrap();
+    let epsilon_rate = usize::from_str_radix(&inv_final_bin, 2).unwrap();
+    gamma_rate * epsilon_rate
+}
+
+
+fn search_value(numbers: &[String], mode: Mode) -> usize {
+    let critical_bit = match mode {
+        Mode::MostCommon => '1',
+        Mode::LeastCommon => '0',
     };
 
-    let gamma_vec: Vec<bool> = lines.
-        iter().
-        fold(pivot, |mut p, line| {
-            line.chars().zip(&mut p).for_each(|(c,p)| {
-                p.push(match c {
-                    '1' => true,
-                    '0' => false,
-                    _ => false,
-                });
-            });
-            p
-        }).
-        into_iter().
-        map(|line| {
-            let count = line.iter().filter(|c| **c).count();
-            count > line.len() / 2
-        }).
-        collect();
-        println!("Gamma: {:?}", gamma_vec);
+    let mut local_numbers: Vec<&String> = numbers.iter().collect();
+
+    for i in 0..numbers[0].len() {
+        if local_numbers.len() == 1 {
+            break;
+        }
+        let count_one = local_numbers
+            .iter()
+            .filter(|x| x[i..].starts_with(critical_bit))
+            .count();
+        let count_zero = local_numbers.len() - count_one;
+
+        let current_bit_mask = match count_one.cmp(&count_zero) {
+            Ordering::Less => '0',
+            Ordering::Equal => critical_bit,
+            Ordering::Greater => '1',
+        };
+
+        local_numbers.drain_filter(|x| x[i..].starts_with(current_bit_mask));
+    }
+
+    usize::from_str_radix(local_numbers[0], 2).unwrap()
+}
+
+fn part2(numbers: &[String]) -> usize {
+    let oxygen = search_value(numbers, Mode::MostCommon); // Get Oxygen Generator Rating
+    let co2 = search_value(numbers, Mode::LeastCommon); // Get CO2 Scrubber Rating
+    oxygen * co2
+}
+
+fn main() {
+    const DATA: &str = include_str!("./day3.input");
+    let result = part1(&parse_input(DATA).unwrap());
+    println!("The answer for part1 is: {}", result);
+    let result2 = part2(&parse_input(DATA).unwrap());
+    println!("The answer for part2 is: {}", result2);
 }
